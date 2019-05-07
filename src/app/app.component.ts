801e905a80b09eafbf821e84e127ff21acb925ba
Benjamin Lepine <benjamin.lepine@epitech.eu>
2019-05-07 18:30:19 +0200

    routing ok, auth ok, guard ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  title = 'My Bookshelves';
+  constructor(){
+    var firebaseConfig = {
+      apiKey: "AIzaSyCJT7Hdn1s__7xq30ZaNKUCUYhVAUDb8O4",
+      authDomain: "bookshelves-30f59.firebaseapp.com",
+      databaseURL: "https://bookshelves-30f59.firebaseio.com",
+      projectId: "bookshelves-30f59",
+      storageBucket: "bookshelves-30f59.appspot.com",
+      messagingSenderId: "837659915947",
+      appId: "1:837659915947:web:f62879e64e20af91"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,20 @@ import { HeaderComponent } from './header/header.component';
 import {AuthService} from './services/auth.service';
 import {BooksService} from './services/books.service';
 import {AuthGuardService} from './services/auth-guard.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {path: 'auth/signup', component: SignupComponent},
+  {path: 'auth/signin', component: SigninComponent},
+  {path: 'books', canActivate: [AuthGuardService], component: BookListComponent},
+  {path: 'books/new', canActivate: [AuthGuardService], component: BookFormComponent},
+  {path: 'books/view:id', canActivate: [AuthGuardService], component: SingleBookComponent},
+  {path: '', redirectTo: 'books', pathMatch: 'full'},
+  {path: '**', redirectTo: 'books'}
+]
 
 @NgModule({
   declarations: [
@@ -23,7 +37,11 @@ import {AuthGuardService} from './services/auth-guard.service';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AuthService,
